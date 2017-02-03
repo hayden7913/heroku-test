7cb7c7798a434018ccea907285361fc298bbda96
hayden7913 <hayden321@gmail.com>
2017-02-03 13:00:57 -0800

    working

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-var express = require('express');
-
-var app = express();
+var express = require('express')
+var app = express()
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080);
-
-exports.app = app;
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
